cb3da71fab0b8db333ceb08c6f9a8d234d15aba6
vinh huynh <vvvhuynh@yahoo.com>
2017-07-07 13:06:42 -0500

    complete DataStore module

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -2,21 +2,30 @@
   // get a reference to the namespace, if it exists.
   var App = window.App || {};
 
+  // DataStore constructor, has empty object data property
   function DataStore() {
-    console.log('running the Datastore function');
+    console.log('DataStore instantiated');
     this.data = {};
   }
 
+  // adds a property (key/value pair) to the data object
   DataStore.prototype.add = function (key, val) {
     this.data[key] = val;
   }
 
+  // retrieves the value of the specified key
   DataStore.prototype.get = function (key) {
     return this.data[key];
   }
 
+  // retrieves the entire object
   DataStore.prototype.getAll = function () {
     return this.data;
+  }
+
+  // removes a property from the object
+  DataStore.prototype.remove = function (key) {
+    delete this.data[key];
   }
 
   // attach DataStore to the App object namespace
