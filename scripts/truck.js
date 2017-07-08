8cde53789d08140f381d04e98271fc72741c9ed7
vinh huynh <vvvhuynh@yahoo.com>
2017-07-08 16:32:35 -0500

    added properties to truck module

--- scripts/truck.js
+++ scripts/truck.js
@@ -2,19 +2,36 @@
 
   var App = window.App || {};
 
-  // db will be a DataStore instance to store orders
-  function Truck(truckId, db) {
+  // orders will be a DataStore instance to store orders
+  function Truck(truckId, dataStoreInstance) {
     this.truckId = truckId;
-    this.orders = db; // the truck instance will have its own DataStore instance
+    this.orders = dataStoreInstance;
   }
 
-  // the Truck instance has a DataStore instance as one of its properties which has access to the DataStore methods
+  // * the Truck instance has a DataStore instance as one of its properties which has access to the DataStore methods
+  // * order will be an object that has an email address as its key and the drink as its value
   Truck.prototype.createOrder = function (order) {
+    console.log('Adding order for: ' + order.emailAddress);
     this.orders.add(order.emailAddress, order)
   }
 
+  // once again using the DataStore property, we use its 'remove' method
   Truck.prototype.deliverOrder = function (customerId) {
     this.orders.remove(customerId);
+  }
+
+  Truck.prototype.printOrders = function () {
+    var customerIdArray = Object.keys(this.orders.getAll());
+
+    console.log('Truck #' + this.truckId + ' has pending orders: ');
+
+    // * inside a callback function, 'this' is not assigned to an object
+    // * recall that functions are also objects - one of the methods of a function object is bind().
+    // * bind takes an object argument and returns a new version of the function with the argument object as the value of 'this'
+    // * outside of the forEach body, 'this' refers to the Truck instance which is what bind(this) refers to
+    customerIdArray.forEach(function(id) {
+      console.log(this.orders.get(id));
+    }.bind(this));
   }
 
   App.Truck = Truck;
