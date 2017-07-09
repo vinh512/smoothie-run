142e56bfbd5e88051b4fee60cef7f40f022c6fdd
vinh huynh <vvvhuynh@yahoo.com>
2017-07-09 11:47:51 -0500

    add submit handler for formhandler

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -1,12 +1,29 @@
 (function(window) {
 
   var App = window.App || {};
+  var $ = window.jQuery;
 
-  function FormHandler() {
-    // code will go here
+  // FormHandler module should be usable with any <form> element
+  function FormHandler(selector) {
+    if (!selector) {
+      throw new Error('No selector provided');
+    }
+    // create a reference to the form element
+    this.$formElement = $(selector);
+
+    // ensures the jQuery wrapped selection contains any matching elements
+    if (this.$formElement.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
   }
 
-  App.FormHandler = FormHander;
+  FormHandler.prototype.addSubmitHandler = function() {
+    this.$formElement.on('submit', function(e) {
+      e.preventDefault();
+    });
+  };
+
+  App.FormHandler = FormHandler;
   window.App = App;
 
 })(window)
