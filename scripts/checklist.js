60b13b99ef98eef15f8930f7a0da176cf11dc9eb
vinh huynh <vvvhuynh@yahoo.com>
2017-07-12 15:31:53 -0500

    append rows of orders to checklist area

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -7,27 +7,45 @@
       throw new Error('No selector provided');
     }
 
+    // creates a reference to the element
     this.$element = $(selector);
+
     if (this.$element.lenght === 0) {
       throw new Error('Could not find element with selector: ' + selector);
     }
   }
 
-  function Row(smoothieOrder) {
-    var $div =('<div></div>', {'data-smoothie-order': 'checkbox', 'class': 'checkbox'});
+  /* Create and assemble the subtree of elements using the order data object passed in */
+  function Row(order) {
+    // create the DOM elements that consists of a row. (HTML tag, object making up attributes)
+    var $div = $('<div></div>', {'data-smoothie-order': 'checkbox', 'class': 'checkbox'});
     var $label = $('<label></label>');
-    var $checkbox = $('<input></input>', {type: checkbox, value: smoothieOrder.emailAddress});
+    var $checkbox = $('<input></input>', {type: 'checkbox', value: order.emailAddress});
 
-    var description = smoothieOrder + ' ';
-    if (smoothieOrder.boost) {
-      description += smoothieOrder.boost + ' ';
+    // create string description of the order
+    var description = order.size + ' ';
+    if (order.boost) {
+      description += order.boost + ' ';
     }
-    description += smoothieOrder.smoothie + ', ';
-    description += ' (' + smoothieOrder.emailAddress + ')';
-    description += ' [' + smoothieOrder.boba + 'x]';
+    description += order.smoothie + ', ';
+    description += ' (' + order.emailAddress + ')';
+    description += ' [' + order.boba + 'x]';
+
+    $label.append($checkbox);
+    $label.append(description);
+    $div.append($label);
+
+    // holds onto the DOM representation of the row in an instance variable
+    this.$element = $div;
   }
 
+  CheckList.prototype.addRow = function (order) {
+    // create a new instance of a row using the 'order' info passed in
+    var rowElement = new Row(order);
 
+    // add the new row instance's $element to property to that of the checklist
+    this.$element.append(rowElement.$element);
+  }
 
   App.CheckList = CheckList;
   window.App = App;
--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,23 @@
 
 (function (window) {
 
-  var FORM_SELECTOR='[data-smoothie-order="form"]';
+  var FORM_SELECTOR ='[data-smoothie-order="form"]';
+  var CHECKLIST_SELECTOR = '[data-smoothie-order="checklist"]';
   var App = window.App || {};
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  var CheckList = App.CheckList;
 
   // * instantiate a new Truck object with id & an instance of the DataStore object as the arguments
   // * myTruck is declared inside of this function. In order to interact with the myTruck object, export it to the global namespace
   var myTruck = new Truck('Truck-01', new DataStore());
   window.myTruck = myTruck;
 
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
   var formHandler = new FormHandler(FORM_SELECTOR);
-  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-  console.log(formHandler);
-
+  formHandler.addSubmitHandler( function (data) {
+    myTruck.createOrder.call(myTruck, data);
+    checkList.addRow.call(checkList, data);
+  });
 })(window);
